b3956f35750c6b0e6d755c5bca8d0a4b80730739
Cheong Lee <clee.hq@gmail.com>
2026-04-02 23:27:04 +0900

    feat: restore exit feedback modal with rating and comment

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import OAuthCompleteTracker from "./components/OAuthCompleteTracker";
 import { ActiveSessionProvider } from "@/hooks/useActiveSession";
+import { FeedbackExitListener } from "@/components/FeedbackModal";
 
 export const metadata = {
   title: "꼬비 (Kkobi)",
@@ -28,7 +29,10 @@ export default function RootLayout({ children }) {
       </head>
       <body className="min-h-screen">
         <OAuthCompleteTracker />
-        <ActiveSessionProvider>{children}</ActiveSessionProvider>
+        <ActiveSessionProvider>
+          <FeedbackExitListener />
+          {children}
+        </ActiveSessionProvider>
       </body>
     </html>
   );
